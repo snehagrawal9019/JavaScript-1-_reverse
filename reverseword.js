7cb3c50f743ec8a1fbb5e29f2918aea52d6feab6
snehagrawal9019 <148084897+snehagrawal9019@users.noreply.github.com>
2023-10-16 20:46:27 +0530

    Add files via upload

---
+++ reverseword.js
@@ -0,0 +1,13 @@
+function reverseWordsInSentence(sentence) {
+    const words = sentence.split(' ');
+    const reversedWords = words.map(word => {
+    return word.split('').reverse().join('');
+    });
+    const reversedSentence = reversedWords.join(' ');
+    return reversedSentence;
+}
+
+const inputSentence = "This is a sunny day";
+const reversedSentence = reverseWordsInSentence(inputSentence);
+console.log("Original sentence: " + inputSentence);
+console.log("Reversed sentence: " + reversedSentence);
